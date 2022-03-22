427efa7651f4411aea38e94c2b247b229a924198
hederjanos <hederjanos@gmail.com>
2022-03-22 11:51:55 +0100

    Create basic project structure

---
+++ src/PuzzleReader.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.List;
+
+public class PuzzleReader {
+
+    public static List<String> createListFromInput(Path path) {
+        try {
+            return Files.readAllLines(path);
+        } catch (IOException io) {
+            System.out.println(io.getMessage());
+            return Collections.emptyList();
+        }
+    }
+}
