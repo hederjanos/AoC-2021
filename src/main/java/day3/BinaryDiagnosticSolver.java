56e11168762032559613164bf97f0e9799e78225
hederjanos <hederjanos@gmail.com>
2022-03-23 12:02:17 +0100

    Solve part 2 of BinaryDiagnosticSolver

--- src/main/java/day3/BinaryDiagnosticSolver.java
+++ src/main/java/day3/BinaryDiagnosticSolver.java
@@ -2,7 +2,10 @@ package day3;
 
 import util.Solver;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class BinaryDiagnosticSolver extends Solver<Integer> {
@@ -13,16 +16,11 @@ public class BinaryDiagnosticSolver extends Solver<Integer> {
 
     @Override
     protected Integer solvePartOne() {
-        int[] mostCommonBits = getMostCommonBitsByPosition();
-        return getConsumption(mostCommonBits);
+        int[] mostCommonBits = getMostCommonBitsByPosition(puzzle);
+        return getResult(getRatingsForConsumption(mostCommonBits));
     }
 
-    @Override
-    protected Integer solvePartTwo() {
-        return null;
-    }
-
-    private int[] getMostCommonBitsByPosition() {
+    private int[] getMostCommonBitsByPosition(List<String> puzzle) {
         int[] countArray = new int[puzzle.get(0).length()];
         puzzle.forEach(line -> addBitsFromLineToCountArray(line, countArray));
         return Arrays.stream(countArray).map(sumOfBits -> (puzzle.size() - sumOfBits <= sumOfBits) ? 1 : 0).toArray();
@@ -32,13 +30,55 @@ public class BinaryDiagnosticSolver extends Solver<Integer> {
         IntStream.range(0, line.length()).forEach(index -> countArray[index] += Character.getNumericValue(line.charAt(index)));
     }
 
-    private int getConsumption(int[] sums) {
+    private List<String> getRatingsForConsumption(int[] mostCommonBits) {
+        List<String> ratings = new ArrayList<>();
         StringBuilder gammaRate = new StringBuilder();
         StringBuilder epsilonRate = new StringBuilder();
-        Arrays.stream(sums).forEach(number -> {
+        Arrays.stream(mostCommonBits).forEach(number -> {
             gammaRate.append(number);
             epsilonRate.append((number == 1) ? 0 : 1);
         });
-        return Integer.parseInt(gammaRate.toString(), 2) * Integer.parseInt(epsilonRate.toString(), 2);
+        ratings.add(gammaRate.toString());
+        ratings.add(epsilonRate.toString());
+        return ratings;
+    }
+
+    private int getResult(List<String> ratings) {
+        return ratings.stream().map(string -> Integer.parseInt(string, 2)).reduce(1, (a, b) -> a * b);
+    }
+
+    @Override
+    protected Integer solvePartTwo() {
+        return getResult(getRatingsForLifeSupportRating());
+    }
+
+    private List<String> getRatingsForLifeSupportRating() {
+        List<String> ratings = new ArrayList<>();
+        String oxygenGeneratorRating = getRating(true);
+        String carbonDioxideScrubberRating = getRating(false);
+        ratings.add(oxygenGeneratorRating);
+        ratings.add(carbonDioxideScrubberRating);
+        return ratings;
+    }
+
+    private String getRating(boolean bitCriteria) {
+        List<String> diagnosticReport = copyPuzzle();
+        int index = 0;
+        while (diagnosticReport.size() > 1) {
+            int[] mostCommonBits = getMostCommonBitsByPosition(diagnosticReport);
+            int finalIndex = index;
+            diagnosticReport = diagnosticReport.stream()
+                    .filter(line -> {
+                        int filterOption = bitCriteria ? mostCommonBits[finalIndex] : (mostCommonBits[finalIndex] == 1) ? 0 : 1;
+                        return Character.getNumericValue(line.charAt(finalIndex)) == filterOption;
+                    })
+                    .collect(Collectors.toList());
+            index++;
+        }
+        return diagnosticReport.stream().findFirst().orElse(null);
+    }
+
+    private List<String> copyPuzzle() {
+        return new ArrayList<>(puzzle);
     }
 }
