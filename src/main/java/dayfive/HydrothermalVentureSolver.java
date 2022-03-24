03ce6b1a4a0073b05b829ae674df78a1ed458a73
hederjanos <hederjanos@gmail.com>
2022-03-24 17:30:17 +0100

    Sorting in progress

--- src/main/java/dayfive/HydrothermalVentureSolver.java
+++ src/main/java/dayfive/HydrothermalVentureSolver.java
@@ -3,9 +3,7 @@ package dayfive;
 import coordinate.Coordinate;
 import util.Solver;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class HydrothermalVentureSolver extends Solver<Integer> {
@@ -16,7 +14,8 @@ public class HydrothermalVentureSolver extends Solver<Integer> {
 
     @Override
     protected Integer solvePartOne() {
-        getInLineCoordinates().forEach(System.out::println);
+        List<Coordinate> coordinates = getInLineCoordinates();
+        Collections.sort(coordinates, (o1, o2) -> o1.getX().compareTo(o2.getX());
         return null;
     }
 
@@ -24,14 +23,14 @@ public class HydrothermalVentureSolver extends Solver<Integer> {
         List<Coordinate> allCoordinates = new ArrayList<>();
         List<List<Coordinate>> listOfCoordinatePairs = parseCoordinates();
         listOfCoordinatePairs.stream()
-                .filter(coordinates -> {
-                    Coordinate start = coordinates.get(0);
-                    Coordinate end = coordinates.get(1);
+                .filter(lines -> {
+                    Coordinate start = lines.get(0);
+                    Coordinate end = lines.get(1);
                     return start.isInHorizontalOrVerticalLineWith(end);
                 })
-                .forEach(coordinates -> {
-                    Coordinate start = coordinates.get(0);
-                    Coordinate end = coordinates.get(1);
+                .forEach(lines -> {
+                    Coordinate start = lines.get(0);
+                    Coordinate end = lines.get(1);
                     allCoordinates.addAll(start.getCoordinatesInLineBetweenCoordinates(end));
                 });
         return allCoordinates;
