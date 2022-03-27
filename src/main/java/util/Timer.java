8aa5302ff5a15718f10bfeccc6d93f2c7db4ca77
hederjanos <hederjanos@gmail.com>
2022-03-27 22:02:16 +0200

    Add Timer class

---
+++ src/main/java/util/Timer.java
@@ -0,0 +1,22 @@
+package util;
+
+public class Timer {
+
+    private long startTime;
+    private long endTime;
+
+    public void start() {
+        startTime = System.nanoTime();
+        endTime = startTime;
+    }
+
+    public void stop() {
+        endTime = System.nanoTime();
+    }
+
+    public void printExecutionTime() {
+        long executionTime = (endTime - startTime);
+        double millis = (double) executionTime / 1000000;
+        System.out.format("Execution time was : %.2f ms.%n", millis);
+    }
+}
