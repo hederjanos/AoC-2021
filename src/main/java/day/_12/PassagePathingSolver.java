5ac818899dbbb851ac40677f823c838395f49075
hederjanos <hederjanos@gmail.com>
2022-11-14 23:50:21 +0100

    Solve part two of PassagePathingSolver

--- src/main/java/day/_12/PassagePathingSolver.java
+++ src/main/java/day/_12/PassagePathingSolver.java
@@ -47,7 +47,49 @@ public class PassagePathingSolver extends Solver<Integer> {
 
     @Override
     protected Integer solvePartTwo() {
-        return null;
+        int numberOfPath = 0;
+        Deque<List<Cave>> stack = new ArrayDeque<>();
+        Cave start = caveSystem.getStart();
+        List<Cave> startList = new ArrayList<>();
+        startList.add(start);
+        stack.push(startList);
+        while (!stack.isEmpty()) {
+            List<Cave> caves = stack.pop();
+            List<Cave> neighbours = caveSystem.getNeighboursOfCave(caves.get(caves.size() - 1).hashCode());
+            for (Cave cave : neighbours) {
+                List<Cave> copy = new ArrayList<>(caves);
+                if (caveSystem.getEnd().equals(cave)) {
+                    numberOfPath++;
+                    continue;
+                }
+                if (!caveSystem.getStart().equals(cave) &&
+                    (cave.isBig() || !copy.contains(cave) || (isCaveOnceIn(cave, copy) && areAllSmallCavesOnceIn(copy)))) {
+                    copy.add(cave);
+                    stack.push(copy);
+                }
+            }
+        }
+        return numberOfPath;
+    }
+
+    private boolean isCaveOnceIn(Cave cave, List<Cave> copy) {
+        return copy.stream().filter(c -> c.equals(cave)).count() == 1;
+    }
+
+    private boolean areAllSmallCavesOnceIn(List<Cave> copy) {
+        boolean areAllSmallCavesOnceIn = true;
+        Map<Cave, Integer> caveMap = new HashMap<>();
+        List<Cave> smallCaves = copy.stream()
+                .filter(cave -> !cave.isBig()).collect(Collectors.toList());
+        for (Cave cave : smallCaves) {
+            if (!caveMap.containsKey(cave)) {
+                caveMap.put(cave, 1);
+            } else {
+                areAllSmallCavesOnceIn = false;
+                break;
+            }
+        }
+        return areAllSmallCavesOnceIn;
     }
 
 }
